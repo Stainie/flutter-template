32d3d67b8635b29e051fd0ef350c63ff8d1b07ae
StankoKostic <stanko@revenyou.io>
2021-01-10 15:43:09 +0100

    small testing changes

--- test/setup/test_helpers.dart
+++ test/setup/test_helpers.dart
@@ -8,18 +8,31 @@ class AuthenticationServiceMock extends Mock implements AuthenticationService {}
 class NavigationServiceMock extends Mock implements NavigationService {}
 
 AuthenticationService getAndRegisterAuthServiceMock() {
+  _removeExistingRegistration<AuthenticationService>();
   var service = AuthenticationServiceMock();
   locator.registerSingleton<AuthenticationService>(service);
   return service;
 }
 
 NavigationService getAndRegisterNavServiceMock() {
+  _removeExistingRegistration<NavigationService>();
   var service = NavigationServiceMock();
   locator.registerSingleton<NavigationService>(service);
   return service;
 }
 
+void registerServices() {
+  getAndRegisterAuthServiceMock();
+  getAndRegisterNavServiceMock();
+}
+
 void unregisterServices() {
   locator.unregister<AuthenticationService>();
   locator.unregister<NavigationService>();
+}
+
+void _removeExistingRegistration<T>() {
+  if (locator.isRegistered<T>()) {
+    locator.unregister<T>();
+  }
 }
--- test/viewmodels/user_viewmodel_test.dart
+++ test/viewmodels/user_viewmodel_test.dart
@@ -4,15 +4,19 @@ import 'package:flutter_test/flutter_test.dart';
 import '../setup/test_helpers.dart';
 
 main() {
-  tearDown(() => unregisterServices());
   group("UserViewModelTest -", () {
+    setUp(() => registerServices());
+    tearDown(() => unregisterServices());
     group("CanAuthenticate -", () {
-      test("When authenticating username should be stanko", () {
+      test("When authenticating username should be tester", () {
+        // Setup - Arrange
         getAndRegisterAuthServiceMock();
         getAndRegisterNavServiceMock();
         var model = UserViewModel();
-        model.setUsername("stanko");
-        expect(model.username, "stanko");
+        // Action - Act
+        model.setUsername("tester");
+        // Result - Assert
+        expect(model.username, "tester");
       });
     });
   });
