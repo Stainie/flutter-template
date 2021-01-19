93b27604b15cdd846c7e1515ac573c541b6cdde5
StankoKostic <stanko@revenyou.io>
2021-01-19 18:26:01 +0100

    base collection, map condition

--- lib/state/app_state.dart
+++ lib/state/app_state.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_template/models/collections/base_collection.dart';
+import 'package:flutter_template/models/mapped_collections/base_mapped_collections.dart';
+
 import '../models/base_model.dart';
 
 class AppState {
@@ -9,7 +12,9 @@ class AppState {
     Map<String, dynamic> clonedState = Map.from(state);
 
     clonedState.forEach((key, value) {
-      if (value is BaseModel) {
+      if (value is BaseModel ||
+          value is BaseCollection ||
+          value is BaseMappedCollections) {
         clonedState[key] = value.clone();
       }
     });
